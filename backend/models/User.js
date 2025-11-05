e5b212c1b44a2fe052ad99c15c6ca946506a9171
Rellinxe27 <bon19013@byui.edu>
2025-11-05 19:21:18 +0000

    feat: add User model for OAuth user data

---
+++ backend/models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  googleId: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  displayName: {
+    type: String,
+    required: true
+  },
+  firstName: {
+    type: String
+  },
+  lastName: {
+    type: String
+  },
+  picture: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  lastLogin: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
